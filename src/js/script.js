db47e5b279c00a787a1b38adf8f878549aa49fe5
doc2Xray <108229558+DoctorBrowser@users.noreply.github.com>
2024-03-22 20:49:05 +0200

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-   $('.carousel__inner').slick();
+   $('.carousel__inner').slick({   /*слайдер*/
+      dots: false,
+      infinite: true,
+      speed: 1500,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplayspeed: 200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"</button>',  /*стрелки вслайдере*/
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"</button>',
+      responsive: [
+         {
+            breakpoint: 992,   /*адаптация екрана*/
+            settings: {
+               dots: true,
+               arrows: false,
+            }
+         },]
+   });
 });
